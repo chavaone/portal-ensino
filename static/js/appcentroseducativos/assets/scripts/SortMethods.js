e5bc4aab6c7c0dfa0c9c70506c26b3a8584c0ceb
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2019-07-16 20:39:43 +0200

    Cambiadas funcións e ordenar para ter en conta o caso onde non hai datos

--- static/js/appcentroseducativos/assets/scripts/SortMethods.js
+++ static/js/appcentroseducativos/assets/scripts/SortMethods.js
@@ -3,9 +3,13 @@ export default {
     return centro1.nome.localeCompare(centro2.nome);
   },
   sortByTime(centro1, centro2) {
-    return centro1.osm.tiempo < centro2.osm.tiempo ? -1 : 1;
+    if (! centro1.osm.tiempo) return 1;
+    if (! centro2.osm.tiempo) return -1;
+    return centro1.osm.tiempo - centro2.osm.tiempo;
   },
   sortByDistance(centro1, centro2) {
-    return centro1.osm.distancia < centro2.osm.distancia ? -1 : 1;
+    if (! centro1.osm.distancia) return 1;
+    if (! centro2.osm.distancia) return -1;
+    return centro1.osm.distancia - centro2.osm.distancia;
   }
 }
